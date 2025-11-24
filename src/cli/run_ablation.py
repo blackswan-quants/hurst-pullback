d0dfcf0eb39b297ddc9fff1620c0b19d68ed06f8
Gabriele Cova <gabriele.covv@gmail.com>
2025-11-24 12:05:38 +0100

    Create run ablation and modify strategy

--- src/cli/run_ablation.py
+++ src/cli/run_ablation.py
@@ -1,3 +1,11 @@
+import yaml
+from src.core.loader import load_data
+from src.core.engine import run
+from src.strategy.strategy import Strategy
+import pandas as pd 
+import copy
+import logging
+
 def main() -> None:
     """
     Run ablation tests to isolate component contributions.
@@ -6,4 +14,38 @@ def main() -> None:
     2. Disable one component at a time.
     3. Re-run backtest and log performance differences.
     """
-    pass
+    with open("./configs/base.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    df = pd.read_csv("./" + config['data']['clean_ES']) 
+    #df = load_data(config['data']['clean_ES'])             !!!!! DA SOSTITUIRE
+
+    config_opt = ['use_hurst', 'use_RSI_exit', 'use_take_profit']
+    output = []
+
+    for opt in config_opt:
+        tmp_config = copy.deepcopy(config)
+        tmp_config['ablation'][opt] = False
+        strategy = Strategy(cfg=tmp_config)
+
+        res = run(df=df, strategy=strategy)
+        logging.info("-" * 50)
+        logging.info("\nDISABLED feature: \t ", opt)
+        out = {
+            "disaled_feature": opt,
+            "output": res
+        }
+        output.append(out)
+        logging.info("\n\n- GENERAL INFOs")
+        logging.info("\nequity -> ", res["equity"])
+        logging.info("\nn_trades -> ", res["n_trades"])
+        logging.info("\ntrades -> ", res["trades"])
+        logging.info("\n\n- METRICS")
+        # metrics QUI
+        logging.info("\n")
+        logging.info("-" * 50) 
+    
+    return
+
+if __name__ == '__main__':
+    main()
--- src/strategy/strategy.py
+++ src/strategy/strategy.py
@@ -19,11 +19,11 @@ class Strategy:
         """
         Initialize strategy with configuration parameters.
         """
-        if 'max_bars_in_trade' not in cfg.keys():
+        if 'max_bars_in_trade' not in cfg['exits'].keys():
             logging.error("The parameters ’max_bars_in_trade’ is NOT in the configuration dictionary!")
-        if 'max_profitable_closes' not in cfg.keys():
+        if 'max_profitable_closes' not in cfg['exits'].keys():
             logging.error("'max_profitable_closes' is NOT in configuration dictionary!")
-        if 'hurst_threshold' not in cfg.keys():
+        if 'hurst_threshold' not in cfg['entry_thresholds'].keys():
             logging.error("Parameters dictionary does NOT contain hurst threshold!")
         self.__cfg = cfg
     def get_cfg(self) -> dict:
@@ -50,24 +50,28 @@ class Strategy:
         Output:
         bool: True if long entry condition is met.
         """
-        long_entry_check = True
+        long_entry_check = False
         # RSI check
-        if 'rsi' in df.columns:
-            if not long_entry(df, i, self.__cfg):
-                long_entry_check = False
-                logging.info("NO ENTRY: RSI indicators was NOT between 10 ad 20.")
-        elif 'rsi' not in df.columns:
-            logging.error('Dataframe does NOT have RSI columns!')
+        if self.__cfg['ablation']['use_rsi']:
+            if 'rsi' in df.columns:
+                if long_entry(df, i, self.__cfg):
+                    long_entry_check = True
+                else:
+                    logging.info("NO ENTRY: RSI indicators was NOT between 10 ad 20.")
+            elif 'rsi' not in df.columns:
+                logging.error('Dataframe does NOT have RSI columns!')
 
         # Hurst check
-        if 'hurst' in df.columns and 'hurst_threshold' in self.__cfg.keys():
-            if long_entry_check and not allow(df, i, self.__cfg):
-                long_entry_check = False
-                logging.info("NO ENTRY: Hurst filter is NOT above the threshold.")
-        elif 'hurst_threshold' not in self.__cfg.keys():
-            logging.error("Parameters dictionary does NOT contain hurst threshold!")
-        elif 'hurst' not in df.columns:
-            logging.error('Hurst exponent is NOT in the dataframe!')
+        if self.__cfg['ablation']['use_hurst'] and not long_entry_check:
+            if 'hurst' in df.columns and 'hurst_threshold' in self.__cfg['entry_thresholds'].keys():
+                if allow(df, i, self.__cfg):
+                    long_entry_check = True
+                else:
+                    logging.info("NO ENTRY: Hurst filter is NOT above the threshold.")
+            elif 'hurst_threshold' not in self.__cfg['entry_thresholds'].keys():
+                logging.error("Parameters dictionary does NOT contain hurst threshold!")
+            elif 'hurst' not in df.columns:
+                logging.error('Hurst exponent is NOT in the dataframe!')
 
         return long_entry_check
     def exit_signal(self, df: pd.DataFrame, i: int, state: dict) -> bool:
@@ -83,28 +87,29 @@ class Strategy:
         exit_position = False
 
         # Time exit check
-        if 'bars' not in state.keys():
-            logging.error("'bars' number is NOT in state dictionary!")
-        elif 'max_bars_in_trade' not in self.__cfg.keys():
-            logging.error("The parameters ’max_bars_in_trade’ is NOT in the configuration dictionary!")
-        elif should_exit(state, self.__cfg):
-            exit_position = True
-            logging.info(f"EXIT SIGNAL: the maximum bars in trade is reached")
+        if self.__cfg['ablation']['use_time_exit']:
+            if 'bars' not in state.keys():
+                logging.error("'bars' number is NOT in state dictionary!")
+            elif 'max_bars_in_trade' not in self.__cfg['exits'].keys():
+                logging.error("The parameters ’max_bars_in_trade’ is NOT in the configuration dictionary!")
+            elif should_exit(state, self.__cfg):
+                exit_position = True
+                logging.info(f"EXIT SIGNAL: the maximum bars in trade is reached")
 
         # pofit exit check
-        if not exit_position:
+        if self.__cfg['ablation']['use_take_profit'] and not exit_position:
             if 'entry_price' not in state.keys():
                 logging.error("'entry_price' is NOT in state dictionary!")
             elif 'prof_x' not in state.keys():
                 logging.error("'prof_x' is NOT in state dictionary!")
-            elif 'max_profitable_closes' not in self.__cfg.keys():
+            elif 'max_profitable_closes' not in self.__cfg['exits'].keys():
                 logging.error("'max_profitable_closes' is NOT in configuration dictionary!")
             elif prof_exit(df, i, state, self.__cfg):
                 exit_position = True
                 logging.info(f"EXIT SIGNAL: the position was profitable for {self.__cfg['max_profitable_closes']} days.")                
 
         # composite rsi check
-        if not exit_position:
+        if self.__cfg['ablation']['use_RSI_exit'] and not exit_position:
             if 'composite_rsi' not in df.colums:
                 logging.error("'composite_rsi' is NOT in the dataframe!")
             elif rsi_exit(df, i, self.__cfg):

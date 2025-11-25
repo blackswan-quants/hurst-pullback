b66f5e0ee0e4aad8094faa6549f6f5c7cef085cd
Riccardo Rotondi <riccardo.rotondi03@gmail.com>
2025-11-25 16:23:43 +0100

    push

--- src/strategy/signals/rsi2.py
+++ src/strategy/signals/rsi2.py
@@ -13,8 +13,11 @@ def long_entry(df: pd.DataFrame, i: int, params: dict) -> bool:
 
     rsi_low = params.get('rsi_low') 
     rsi_high = params.get('rsi_high')
+ 
+    if(rsi_low is None or pd.isna(rsi_low) or 
+       rsi_high is None or pd.isna(rsi_high) or 
+       curr_rsi is None or pd.isna(curr_rsi)
+    ):
+        return False
 
-    if(pd.isna(curr_rsi)): 
-        return False 
-    
-    return rsi_low <= curr_rsi and curr_rsi <= rsi_high
+    return rsi_low <= curr_rsi <= rsi_high
